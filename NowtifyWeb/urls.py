fa5eea8c751139474cf75af1c98c73bed5ac70a8
afiq980 <afiq980@gmail.com>
2016-07-06 10:55:05 +0800

    Added log out functionality

--- NowtifyWeb/urls.py
+++ NowtifyWeb/urls.py
@@ -7,9 +7,9 @@ urlpatterns = patterns('',
                        url(r'^index/', views.index, name='index'),
                        url(r'^overview/', views.overview, name='overview'),
                        url(r'^sensor/', views.sensor, name='sensor'),
-                       url(r'^logout/', views.logout, name='logout')
+                       url(r'^logout/', views.logout, name='logout'),
                        url(r'^wearable/', views.wearable, name='wearable'),
-                       url(r'^dashboard/', views.dashboard, name='dashboard')
+                       url(r'^dashboard/', views.dashboard, name='dashboard'),
                        url(r'^wearable/', views.wearable, name='wearable'),
                        url(r'^dashboard/', views.dashboard, name='dashboard'),
                        url(r'^change_password/', views.settings, name='settings'),
@@ -24,4 +24,3 @@ urlpatterns = patterns('',
                        # url(r'^dashboard/', views.dashboard, name='dashboard')
 
                        )
-
