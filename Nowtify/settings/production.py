456974cd067d31eed8fda4e6febd800a283832b9
afiq980 <afiq980@gmail.com>
2016-07-21 07:33:59 +0800

    uhhuh

--- Nowtify/settings/production.py
+++ Nowtify/settings/production.py
@@ -97,7 +97,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'NowtifyWeb'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/'
 
