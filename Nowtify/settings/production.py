8a9ee676df2acb55c29903207559f16eab05427a
afiq980 <afiq980@gmail.com>
2016-07-21 07:35:59 +0800

    this is life

--- Nowtify/settings/production.py
+++ Nowtify/settings/production.py
@@ -104,7 +104,7 @@ LOGIN_URL = '/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    "NowtifyWeb/static",
 )
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
