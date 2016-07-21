43d901cb29e13695a11f62808d9d13bfe8c2b0fa
afiq980 <afiq980@gmail.com>
2016-07-21 10:05:08 +0800

    wer

--- Nowtify/wsgi.py
+++ Nowtify/wsgi.py
@@ -14,10 +14,10 @@ from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
 
-try:
-    from dj_static import Cling
-    application = Cling(get_wsgi_application())
-except:
-    pass
+#try:
+    #from dj_static import Cling
+    #application = Cling(get_wsgi_application())
+#except:
+ #   pass
