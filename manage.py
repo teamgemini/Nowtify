2ace3c26a4425b52b7dfb412b879a0fadbe76d32
afiq980 <afiq980@gmail.com>
2016-07-21 10:23:18 +0800

    asd
    as

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.contrib.sites.models import Site
+
+Site.objects.create(domain='https://nowtify-dev.herokuapp.com', name='https://nowtify-dev.herokuapp.com')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Nowtify.settings")
