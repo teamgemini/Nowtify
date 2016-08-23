bcb0daed1f80dde4c40679e2bd220aee1554e458
afiq980 <afiq980@gmail.com>
2016-08-23 14:36:02 +0800

    deployment

--- Nowtify/views.py
+++ Nowtify/views.py
@@ -312,7 +312,7 @@ def update_assignment(request):
             action = "Battery under 30"
 
         wearableData.append(
-            [str(wearableObject.name), wearableAssignment[count], usage, "Center " + str(wearableLocation[count]),
+            [str(wearableObject.name), str(wearableAssignment[count]), usage, "Center " + str(wearableLocation[count]),
              str(wearableBattery[count]) + "%",
              action, (str(wearableUpdated[count]))[:19]])
         count += 1
@@ -324,7 +324,7 @@ def update_assignment(request):
     for instance in wearableUnique:
         instanceName = instance.name
         if Assignment.objects.all().filter(wearable_name__exact=instance).exists():
-            instanceAssignee = Assignment.objects.all().filter(wearable_name__exact=instance).first().name
+            instanceAssignee = str(Assignment.objects.all().filter(wearable_name__exact=instance).first().name)
         else:
             instanceAssignee = "Not Assigned"
         wearableAssignment.append([instanceName, instanceAssignee])
